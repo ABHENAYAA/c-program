7d8c3115434ae4f5783ce502ead2b35d4af24eb9
ABHENAYAA <abheluba29@gmail.com>
2016-07-08 06:05:26 +0530

    Create sums.c

---
+++ sums.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio,h>
+void main()
+{
+int n;
+int sum=0;
+printf("\n enter the number  :  ");
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+sum=sum+i;
+printf("the sum is  :  %d",sum);
+getch();
+}
+
+    Status API Training Shop Blog About 
+
+    © 2016 GitHub, Inc. Terms
